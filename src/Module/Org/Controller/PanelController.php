94e7f8928ad7526c3ef014ebf84c20ecbc782682
Riley de Man <rileydeman@gmail.com>
2026-04-08 12:28:14 +0200

    Routing Organization Panel ready

--- src/Module/Org/Controller/PanelController.php
+++ src/Module/Org/Controller/PanelController.php
@@ -12,6 +12,87 @@ final class PanelController extends AbstractController
     #[Route('', name: 'home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/main/index.html.twig');
+        return $this->render('pages/org/index.html.twig');
+    }
+
+    #[Route('/projects', name: 'projects', methods: ['GET'])]
+    public function projects(): Response
+    {
+        return $this->render('pages/org/projects.html.twig');
+    }
+
+    #[Route('/projects/create', name: 'createProject', methods: ['GET'])]
+    public function createProject(): Response
+    {
+        return $this->render('pages/org/projects/create.html.twig');
+    }
+
+    #[Route('/projects/modify/{id}', name: 'modifyProject', methods: ['GET'])]
+    public function modifyProject(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify.html.twig', [
+            'id' => $id,
+        ]);
+    }
+    #[Route('/projects/modify/{id}/matching', name: 'modifyProject.matching', methods: ['GET'])]
+    public function modifyProjectMatching(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify-matching.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/projects/modify/{id}/participants', name: 'modifyProject.participants', methods: ['GET'])]
+    public function modifyProjectParticipants(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify-participants.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/projects/modify/{id}/reviews', name: 'modifyProject.reviews', methods: ['GET'])]
+    public function modifyProjectReviews(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify-reviews.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/projects/modify/{id}/tasks', name: 'modifyProject.tasks', methods: ['GET'])]
+    public function modifyProjectTasks(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify-tasks.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/projects/modify/{id}/updates', name: 'modifyProject.updates', methods: ['GET'])]
+    public function modifyProjectUpdates(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify-updates.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/projects/modify/{id}/work-packages', name: 'modifyProject.workPackages', methods: ['GET'])]
+    public function modifyProjectWorkPackages(string $id): Response
+    {
+        return $this->render('pages/org/projects/modify-work-packages.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    #[Route('/staff', name: 'staff', methods: ['GET'])]
+    public function staff(): Response
+    {
+        return $this->render('pages/org/staff.html.twig');
+    }
+
+    #[Route('/staff/{id}', name: 'viewStaff', methods: ['GET'])]
+    public function viewStaff(string $id): Response
+    {
+        return $this->render('pages/org/staff/view.html.twig', [
+            'id' => $id,
+        ]);
     }
 }
