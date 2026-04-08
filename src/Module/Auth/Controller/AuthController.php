75b90839d0ffe1b64c5c863c35bf4e1bdbc36206
Riley de Man <rileydeman@gmail.com>
2026-04-08 10:49:30 +0200

    Routing ready for Authentication pages

--- src/Module/Auth/Controller/AuthController.php
+++ src/Module/Auth/Controller/AuthController.php
@@ -9,9 +9,30 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/auth', name: 'auth.')]
 final class AuthController extends AbstractController
 {
-    #[Route('', name: 'home', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/login', name: 'login', methods: ['GET'])]
+    public function login(): Response
     {
-        return $this->render('pages/main/index.html.twig');
+        return $this->render('pages/auth/login.html.twig');
+    }
+
+    #[Route('/register', name: 'register', methods: ['GET'])]
+    public function register(): Response
+    {
+        return $this->render('pages/auth/register.html.twig');
+    }
+
+    #[Route('/forgot-password', name: 'forgotPassword', methods: ['GET'])]
+    public function forgotPassword(): Response
+    {
+        return $this->render('pages/auth/forgot-password.html.twig');
+    }
+
+    #[Route('/reset-password/{token}/{email}', name: 'resetPassword', methods: ['GET'])]
+    public function resetPassword(string $token, string $email): Response
+    {
+        return $this->render('emails/reset-password.html.twig',[
+            'token' => $token,
+            'email' => $email
+        ]);
     }
 }
