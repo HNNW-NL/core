b81a38cb052b4a2e90956b46a5af36a8d4dcaf05
Riley de Man <rileydeman@gmail.com>
2026-04-08 11:55:32 +0200

    Routing ready for the social/forum

--- src/Module/Social/Controller/SocialController.php
+++ src/Module/Social/Controller/SocialController.php
@@ -12,6 +12,42 @@ final class SocialController extends AbstractController
     #[Route('', name: 'home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/main/index.html.twig');
+        return $this->render('pages/social/index.html.twig');
+    }
+
+    #[Route('/categories', name: 'categories', methods: ['GET'])]
+    public function categories(): Response
+    {
+        return $this->render('pages/social/categories.html.twig');
+    }
+
+    #[Route('/categories/{id}', name: 'viewCategory', methods: ['GET'])]
+    public function viewCategory(string $id): Response
+    {
+        return $this->render('pages/social/categories-view.html.twig', [
+            'id' => $id
+        ]);
+    }
+
+    #[Route('/create-post', name: 'createPost', methods: ['GET'])]
+    public function createPost(): Response
+    {
+        return $this->render('pages/social/posts/create.html.twig');
+    }
+
+    #[Route('/p/{id}', name: 'viewPost', methods: ['GET'])]
+    public function viewPost(string $id): Response
+    {
+        return $this->render('pages/social/posts/view.html.twig', [
+            'id' => $id
+        ]);
+    }
+
+    #[Route('/p/{id}/modify', name: 'modifyPost', methods: ['GET'])]
+    public function modifyPost(string $id): Response
+    {
+        return $this->render('pages/social/posts/modify.html.twig', [
+            'id' => $id
+        ]);
     }
 }
