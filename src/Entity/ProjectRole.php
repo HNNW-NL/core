682ba57d7365af17eda070b00bf94842963a35d1
CaylenRamazan <9023381@student.zadkine.nl>
2026-04-19 15:44:29 +0200

    toegevoegd ProjectParticipant Entity, ProjectRole Entity en Controller en twig daar op aangepast.

---
+++ src/Entity/ProjectRole.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'project_roles')]
+class ProjectRole
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private string $name;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: 'json', nullable: true)]
+    private ?array $permFields = [];
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTimeInterface $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTimeInterface $lastModified;
+
+    // ========================
+    // Getters & Setters
+    // ========================
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getPermFields(): ?array
+    {
+        return $this->permFields;
+    }
+
+    public function setPermFields(?array $permFields): self
+    {
+        $this->permFields = $permFields;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getLastModified(): \DateTimeInterface
+    {
+        return $this->lastModified;
+    }
+
+    public function setLastModified(\DateTimeInterface $lastModified): self
+    {
+        $this->lastModified = $lastModified;
+        return $this;
+    }
+}
--- src/Module/Org/Controller/PanelController.php
+++ src/Module/Org/Controller/PanelController.php
@@ -2,9 +2,12 @@
 
 namespace App\Module\Org\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\ProjectRole;
+use App\Entity\ProjectParticipant;
 
 #[Route('/org', name: 'org.')]
 final class PanelController extends AbstractController
@@ -43,10 +46,26 @@ final class PanelController extends AbstractController
     }
 
     #[Route('/projects/modify/{id}/participants', name: 'modifyProject.participants', methods: ['GET'])]
-    public function modifyProjectParticipants(string $id): Response
+    public function modifyProjectParticipants(string $id, EntityManagerInterface $em): Response
     {
+        $qb = $em->createQueryBuilder();
+
+        $qb->select('pp', 'a', 'currentRole')
+            ->from(ProjectParticipant::class, 'pp')
+            ->join('pp.account', 'a')
+            ->join('pp.role', 'currentRole')
+            ->where('pp.project = :projectId')
+            ->setParameter('projectId', $id);
+
+        $participants = $qb->getQuery()->getResult();
+
+        $allRoles = $em->getRepository(ProjectRole::class)->findAll();
+
+
         return $this->render('pages/org/projects/modify-participants.html.twig', [
             'id' => $id,
+            'participants' => $participants,
+            'roles' => $allRoles,
         ]);
     }
 
