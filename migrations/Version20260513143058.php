9ff5ceaffa3fd3107b367cc2488a7e01d59ff0b1
Riley de Man <rileydeman@gmail.com>
2026-05-13 16:32:57 +0200

    Feat: Updated migrations 13/05/2026.

--- migrations/Version20260513143058.php
+++ migrations/Version20260513143058.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260512125343 extends AbstractMigration
+final class Version20260513143058 extends AbstractMigration
 {
     public function getDescription(): string
     {
