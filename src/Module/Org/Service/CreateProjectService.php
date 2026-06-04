a656bf3eefbcf7c35d0612470a997a239e9e8a4b
yusuf38424 <9015152@student.zadkine.nl>
2026-06-04 13:04:00 +0200

    service update

--- src/Module/Org/Service/CreateProjectService.php
+++ src/Module/Org/Service/CreateProjectService.php
@@ -2,8 +2,11 @@
 
 namespace App\Module\Org\Service;
 
-use App\Module\Org\DTO\CreateProjectDTO;
+use App\Entity\Account\Account;
+use App\Entity\Common\Status;
+use App\Entity\Org\Organisation;
 use App\Entity\Project\Project;
+use App\Module\Org\DTO\CreateProjectDTO;
 use Doctrine\ORM\EntityManagerInterface;
 
 class CreateProjectService
@@ -15,28 +18,33 @@ class CreateProjectService
     public function create(CreateProjectDTO $dto): Project
     {
         $project = new Project();
+
         $project->setTitle($dto->name);
-        $project->setSummary($dto->summary);
+        $project->setSummary($dto->summary);    
         $project->setDescription($dto->description);
         $project->setCapacity($dto->capacity);
         $project->setVisibility($dto->visibility);
 
         $slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $dto->name)));
         $project->setSlug($slug);
+
         $project->setStartDate(new \DateTimeImmutable());
-        $project->setEndDate($dto->endDate);
-        if ($dto->remotePossible !== null) {
-        $project->enableRemoteWork();
-}
-        if ($dto->publishedAt !== null) {
-        $project->publish();
-}
-        if ($dto->deletedAt !== null) {
-        $project->softDelete();
-}
-        $project->setOwnerAccount($dto->ownerAccount);
-        $project->setOwnerOrganisation($dto->ownerOrganisation);
-        $project->setStatus($dto->status);
+
+        if ($dto->endDate !== null) {
+            $project->setEndDate($dto->endDate);
+        }
+
+        $account = $this->entityManager->getRepository(Account::class)->findOneBy([]);
+        $organisation = $this->entityManager->getRepository(Organisation::class)->findOneBy([]);
+        $status = $this->entityManager->getRepository(Status::class)->findOneBy([]);
+
+        if (!$account || !$organisation || !$status) {
+            throw new \RuntimeException('No valid Account/Organisation/Status found in DB');
+        }
+
+        $project->setOwnerAccount($account);
+        $project->setOwnerOrganisation($organisation);
+        $project->setStatus($status);
 
         $this->entityManager->persist($project);
         $this->entityManager->flush();
