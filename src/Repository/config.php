237f41dd0988c9849247da770668462fa6fbb576
Kaizen <emir5314@outlook.com>
2026-06-09 12:44:13 +0200

    organisaties inzien, aanmaken en bijwerken. (nog niet klaar)

---
+++ src/Repository/config.php
@@ -0,0 +1,12 @@
+<?php
+// auteur: Emirhan
+// functie: configuratiebestand
+
+define("DATABASE", "hnnw_care");
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+define("CRUD_TABLE", "Users");
+
+?>
