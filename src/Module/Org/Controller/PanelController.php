33640a54ed9af2491216464e5284e4b64c285258
Erdemakoz <9023025@tcrmbo.nl>
2026-06-11 17:41:36 +0200

    Functionaliteit voor werkpakketten bij projecten toegevoegd

--- src/Module/Org/Controller/PanelController.php
+++ src/Module/Org/Controller/PanelController.php
@@ -2,10 +2,15 @@
 
 namespace App\Module\Org\Controller;
 
+use App\Entity\Common\Status;
+use App\Entity\Project\Project;
+use App\Entity\Project\WorkPackage;
+use App\Repository\Project\WorkPackageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\Project\WorkPackageRepository;
 
 #[Route('/org', name: 'org.')]
 final class PanelController extends AbstractController
@@ -80,12 +85,42 @@ final class PanelController extends AbstractController
         '/projects/modify/{id}/work-packages',
         name: 'modifyProject.workPackages',
         requirements: ['id' => '[0-9a-fA-F-]{36}'],
-        methods: ['GET']
+        methods: ['GET', 'POST']
     )]
     public function modifyProjectWorkPackages(
         string $id,
+        Request $request,
+        EntityManagerInterface $entityManager,
         WorkPackageRepository $workPackageRepository
     ): Response {
+        if ($request->isMethod('POST')) {
+            $project = $entityManager->getRepository(Project::class)->find($id);
+            $status = $entityManager->getRepository(Status::class)->findOneBy([]);
+
+            if ($project !== null && $status !== null) {
+                $title = (string) $request->request->get('title', '');
+                $description = (string) $request->request->get('description', '');
+
+                $slug = strtolower(trim($title));
+                $slug = preg_replace('/[^a-z0-9]+/', '-', $slug);
+                $slug = trim((string) $slug, '-');
+
+                $workPackage = new WorkPackage();
+                $workPackage->setProject($project);
+                $workPackage->setStatus($status);
+                $workPackage->setTitle($title);
+                $workPackage->setSlug($slug);
+                $workPackage->setDescription($description);
+
+                $entityManager->persist($workPackage);
+                $entityManager->flush();
+            }
+
+            return $this->redirectToRoute('org.modifyProject.workPackages', [
+                'id' => $id,
+            ]);
+        }
+
         $workPackages = $workPackageRepository->findActiveByProjectId($id);
 
         return $this->render('pages/org/projects/modify-work-packages.html.twig', [
