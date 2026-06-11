9930eb91bc27199281e45323d6db361460b451f3
Erdemakoz <9023025@tcrmbo.nl>
2026-06-11 10:26:55 +0200

    Revert "Herstel Doctrine relaties voor projecten en werkpakketten"
    This reverts commit d59c90942a2a569cfbab2a10ce762c67502d2b07.

--- src/Entity/Project/ProjectUpdate.php
+++ src/Entity/Project/ProjectUpdate.php
@@ -11,6 +11,7 @@ use Symfony\Component\Uid\Uuid;
 #[ORM\HasLifecycleCallbacks]
 class ProjectUpdate
 {
+    // Columns
 
     #[ORM\Id]
     #[ORM\Column(name: 'id', type: UuidType::NAME, unique: true)]
@@ -44,6 +45,14 @@ class ProjectUpdate
 
     #[ORM\Column(name: 'deleted_at', type: 'datetime_immutable', nullable: true)]
     private ?\DateTimeImmutable $deletedAt = null;
+
+
+    // Reverse FKs
+
+    /* Insert reverse FKs here */
+
+
+    // Functions
 
     public function __construct()
     {
@@ -99,6 +108,9 @@ class ProjectUpdate
     {
         $this->deletedAt = null;
     }
+
+
+    /// Getters & Setters Functions
 
     public function getProject(): ?Project
     {
