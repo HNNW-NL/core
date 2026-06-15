6ee57c7bd247e6ef7d4fc947eb7f9a16bebecf5d
Kaizen <emir5314@outlook.com>
2026-06-15 14:30:40 +0200

    CRUD system

---
+++ src/Repository/config.php
@@ -0,0 +1,13 @@
+<?php
+// auteur: Emirhan
+// functie: configuratiebestand
+
+define("DATABASE", "hnnw_care");
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+// Tabel voor organisaties
+define("CRUD_TABLE", "Organisaties");
+
+?>
