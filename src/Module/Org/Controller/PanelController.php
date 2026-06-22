1bea579ec1508a3a006a3877b27393213e3bdef4
CaylenRamazan <9023381@student.zadkine.nl>
2026-06-22 11:13:31 +0200

    reset page and deleted auto complete field and form

--- src/Module/Org/Controller/PanelController.php
+++ src/Module/Org/Controller/PanelController.php
@@ -2,7 +2,6 @@
 
 namespace App\Module\Org\Controller;
 
-use App\Twig\Components\Org\InviteParticipantsForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -56,13 +55,8 @@ final class PanelController extends AbstractController
     #[Route('/projects/modify/{id}/participants/invite', name: 'modifyProject.inviteParticipants', methods: ['GET'])]
     public function InviteProjectParticipants(string $id, Request $request): Response
     {
-        $form = $this->createForm(InviteParticipantsForm::class);
-
-        $form->handleRequest($request);
-
         return $this->render('pages/org/projects/invite-participants.html.twig', [
             'id' => $id,
-            'form' => $form->createView(),
         ]);
     }
 
