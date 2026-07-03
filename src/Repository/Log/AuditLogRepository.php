088d7aff3375b25beff707c06e8d42d7c4a1f695
Jayden <9022490@student.tcrmbo.nl>
2026-07-03 15:23:55 +0200

    Nieuw bestand toegevoegd

---
+++ src/Repository/Log/AuditLogRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository\Log;
+
+use App\Entity\Log\AuditLog;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class AuditLogRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, AuditLog::class);
+    }
+
+    public function findLatest(int $limit = 5): array
+    {
+        return $this->createQueryBuilder('auditLog')
+            ->orderBy('auditLog.createdAt', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
+}
