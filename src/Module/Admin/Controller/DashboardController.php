e03774b3d4bb41fb257bb30a0e0c189efcf1cb78
rdmge <rileydeman@gmail.com>
2026-07-09 16:06:38 +0200

    Feat: Resetted some files due to composer errors.

--- src/Module/Admin/Controller/DashboardController.php
+++ src/Module/Admin/Controller/DashboardController.php
@@ -2,12 +2,7 @@
 
 namespace App\Module\Admin\Controller;
 
-use App\Module\Admin\DTO\StatusDTO;
-use App\Module\Admin\Handler\CreateStatusHandler;
-use App\Module\Admin\Service\StatusService;
-use App\Module\Admin\Service\AuditLogService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -45,89 +40,8 @@ final class DashboardController extends AbstractController
     }
 
     #[Route('/statuses', name: 'statuses', methods: ['GET'])]
-    public function statuses(
-        StatusService $statusService
-    ): Response {
-        return $this->render('pages/admin/statuses.html.twig', [
-            'statuses' => $statusService->getAllStatuses(),
-        ]);
-    }
-
-    #[Route('/statuses/create', name: 'statuses.create', methods: ['GET', 'POST'])]
-    public function createStatus(
-        Request $request,
-        CreateStatusHandler $handler
-    ): Response {
-        if ($request->isMethod('POST')) {
-
-            $dto = new StatusDTO(
-                $request->request->get('name'),
-                $request->request->get('colourHex'),
-                $request->request->get('scope')
-            );
-
-            $handler->handle($dto);
-
-            return $this->redirectToRoute('admin.statuses');
-        }
-
-        return $this->render('pages/admin/statuses-create.html.twig');
-    }
-
-    #[Route('/statuses/{id}/edit', name: 'statuses.edit', methods: ['GET', 'POST'])]
-    public function editStatus(
-        string $id,
-        Request $request,
-        StatusService $statusService
-    ): Response {
-        $status = $statusService->getStatusById($id);
-
-        if (!$status) {
-            throw $this->createNotFoundException();
-        }
-
-        if ($request->isMethod('POST')) {
-
-            $dto = new StatusDTO(
-                $request->request->get('name'),
-                $request->request->get('colourHex'),
-                $request->request->get('scope')
-            );
-
-            $statusService->updateStatus($id, $dto);
-
-            return $this->redirectToRoute('admin.statuses');
-        }
-
-    // Tijdelijke testroute
-    #[Route('/test-audit-log', name: 'test.audit.log', methods: ['GET'])]
-    public function testAuditLog(AuditLogService $auditLogService): Response
+    public function statuses(): Response
     {
-        $auditLogService->log(
-            null,
-            'Admin Test',
-            'admin@test.nl',
-            'TEST',
-            'System',
-            '1',
-            [],
-            ['message' => 'Audit log test werkt']
-        );
-
-        return new Response('Audit log aangemaakt');
+        return $this->render('pages/admin/statuses.html.twig');
     }
-        return $this->render('pages/admin/statuses-edit.html.twig', [
-            'status' => $status,
-        ]);
-    }
-
-    #[Route('/statuses/{id}/delete', name: 'statuses.delete', methods: ['POST'])]
-     public function deleteStatus(
-    string $id,
-    StatusService $statusService
-    ): Response {
-    $statusService->deleteStatus($id);
-
-    return $this->redirectToRoute('admin.statuses');
-}
 }
--- src/Module/Org/Controller/PanelController.php
+++ src/Module/Org/Controller/PanelController.php
@@ -2,27 +2,9 @@
 
 namespace App\Module\Org\Controller;
 
-use App\Repository\Project\ProjectRoleRepository;
-use App\Module\Org\Handler\ProjectParticipantUpdateRoleHandler;
-use App\Module\Org\DTO\CreateProjectDTO;
-use App\Module\Org\Service\CreateProjectService;
-use App\Entity\Account\Account;
-use App\Module\Org\Handler\ModifyProjectHandler;
-use App\Module\Org\Service\ModifyProjectService;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Project\Project;
-use App\Module\Admin\DTO\CreatePackageTaskDTO;
-use App\Module\Admin\DTO\CreateWorkPackageDTO;
-use App\Module\Admin\Handler\CreatePackageTaskHandler;
-use App\Module\Admin\Handler\CreateWorkPackageHandler;
-use App\Module\Admin\Handler\DeleteWorkPackageHandler;
-use App\Module\Org\Handler\GetOrgProjectWorkPackagesHandler;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Uid\Uuid;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/org', name: 'org.')]
 final class PanelController extends AbstractController
@@ -39,50 +21,18 @@ final class PanelController extends AbstractController
         return $this->render('pages/org/projects.html.twig');
     }
 
-    #[Route('/projects/create', name: 'createProject', methods: ['GET', 'POST'])]
-    public function createProject(Request $request, CreateProjectService $service): Response {
-        if ($request->isMethod('POST')) {
-            $dto = new CreateProjectDTO();
-
-            $dto->name = $request->request->get('name');
-            $dto->summary = $request->request->get('summary');
-            $dto->description = $request->request->get('description');
-            $dto->capacity = (int) $request->request->get('capacity');
-            $dto->visibility = $request->request->get('visibility');
-
-            $service->create($dto);
-
-            return $this->redirectToRoute('org.projects');
-        }
-
-    #[Route('/projects/create', name: 'createProject', methods: ['GET', 'POST'])]
+    #[Route('/projects/create', name: 'createProject', methods: ['GET'])]
     public function createProject(): Response
     {
         return $this->render('pages/org/projects/create.html.twig');
     }
 
-    #[Route('/projects/modify/{id}', name: 'modifyProject', methods: ['GET', 'POST'])]
-    public function modifyProject(
-        string $id,
-        Request $request,
-        ModifyProjectService $projectService,
-        ModifyProjectHandler $handler
-    ): Response
+    #[Route('/projects/modify/{id}', name: 'modifyProject', methods: ['GET'])]
+    public function modifyProject(string $id): Response
     {
-        $publisher = $this->getUser();
-
-        $payload = $projectService->buildResponsePayload(
-            $id,
-            $request,
-            $handler,
-            $publisher instanceof Account ? $publisher : null
-        );
-
-        if (isset($payload['redirect'])) {
-            return $this->redirectToRoute($payload['redirectRoute'] ?? 'org.modifyProject', $payload['redirect']);
-        }
-
-        return $this->render('pages/org/projects/modify.html.twig', $payload['view']);
+        return $this->render('pages/org/projects/modify.html.twig', [
+            'id' => $id,
+        ]);
     }
 
     #[Route('/projects/modify/{id}/matching', name: 'modifyProject.matching', methods: ['GET'])]
@@ -93,53 +43,17 @@ final class PanelController extends AbstractController
         ]);
     }
 
-    #[Route('/projects/modify/{id}/participants', name: 'modifyProject.participants')]
-    public function modifyProjectParticipants(string $id, Request $request, ProjectParticipantRepository $participantRepository,
-                                              ProjectRoleRepository $projectRoleRepository, ProjectParticipantUpdateRoleHandler $updateRoleHandler ): Response
+    #[Route('/projects/modify/{id}/participants', name: 'modifyProject.participants', methods: ['GET'])]
+    public function modifyProjectParticipants(string $id): Response
     {
-        if ($request->isMethod('POST')) {
-            $updateRoleHandler->handle($request->request->all('role'));
-        }
-
-        $participants = $participantRepository
-            ->findByProjectIdWithProfileAndStatus($id);
-
-        $roles = $projectRoleRepository
-            ->findByProjectIdRoles($id);
-
         return $this->render('pages/org/projects/modify-participants.html.twig', [
             'id' => $id,
-            'participants' => $participants,
-            'roles' => $roles,
         ]);
     }
 
     #[Route('/projects/modify/{id}/participants/invite', name: 'modifyProject.inviteParticipants', methods: ['GET'])]
-    public function inviteProjectParticipants(string $id): Response
+    public function InviteProjectParticipants(string $id): Response
     {
-        $query = $request->query->get('q');
-
-        $participants = [];
-
-        if ($request->isXmlHttpRequest() && $query) {
-            $participants = $inviteParticipantRepository->searchByDisplayName($query, 20);
-
-            return new JsonResponse(array_map(fn($p) => [
-                'id' => $p['id'],
-                'displayName' => $p['displayName'],
-            ], $participants));
-        }
-
-        if ($request->isMethod('POST')) {
-
-            $items = $request->request->all('items');
-
-            foreach ($items as $item) {
-                $profileId = $item['id'] ?? null;
-                $displayName = $item['displayName'] ?? null;
-            }
-        }
-
         return $this->render('pages/org/projects/invite-participants.html.twig', [
             'id' => $id,
         ]);
@@ -169,78 +83,11 @@ final class PanelController extends AbstractController
         ]);
     }
 
-    #[Route('/projects/modify/{id}/work-packages', name: 'modifyProject.workPackages', methods: ['GET', 'POST'])]
-    public function modifyProjectWorkPackages(
-        string $id,
-        Request $request,
-        GetOrgProjectWorkPackagesHandler $handler,
-        CreateWorkPackageHandler $createWorkPackageHandler,
-        DeleteWorkPackageHandler $deleteWorkPackageHandler,
-        CreatePackageTaskHandler $createPackageTaskHandler,
-        EntityManagerInterface $entityManager,
-    ): Response {
-        if ($request->isMethod('POST')) {
-            $action = (string) $request->request->get('_action');
-
-            if ($action === 'delete_work_package') {
-                $deleteWorkPackageHandler->handle((string) $request->request->get('workPackageId'));
-
-                return $this->redirectToRoute('org.modifyProject.workPackages', [
-                    'id' => $id,
-                ]);
-            }
-
-            if ($action === 'create_package_task') {
-                $dueDateValue = $request->request->get('taskDueDate');
-
-                $dto = new CreatePackageTaskDTO(
-                    workPackageId: (string) $request->request->get('workPackageId'),
-                    title: (string) $request->request->get('taskTitle'),
-                    slug: (string) $request->request->get('taskSlug'),
-                    description: $request->request->get('taskDescription') ?: null,
-                    dueDate: $dueDateValue ? new \DateTimeImmutable($dueDateValue) : null,
-                    priority: (string) ($request->request->get('taskPriority') ?: 'normal'),
-                );
-
-                $createPackageTaskHandler->handle($dto);
-
-                return $this->redirectToRoute('org.modifyProject.workPackages', [
-                    'id' => $id,
-                ]);
-            }
-
-            if ($action === 'create_work_package') {
-                $dueDateValue = $request->request->get('dueDate');
-
-                $dto = new CreateWorkPackageDTO(
-                    projectId: $id,
-                    title: (string) $request->request->get('title'),
-                    slug: (string) $request->request->get('slug'),
-                    description: $request->request->get('description') ?: null,
-                    dueDate: $dueDateValue ? new \DateTimeImmutable($dueDateValue) : null,
-                );
-
-                $createWorkPackageHandler->handle($dto);
-
-                return $this->redirectToRoute('org.modifyProject.workPackages', [
-                    'id' => $id,
-                ]);
-            }
-        }
-
-        $overview = $handler->handle($id);
-        $project = Uuid::isValid($id)
-            ? $entityManager->find(Project::class, Uuid::fromString($id))
-            : null;
-
+    #[Route('/projects/modify/{id}/work-packages', name: 'modifyProject.workPackages', methods: ['GET'])]
+    public function modifyProjectWorkPackages(string $id): Response
+    {
         return $this->render('pages/org/projects/modify-work-packages.html.twig', [
             'id' => $id,
-            'projectTitle' => $project?->getTitle(),
-            'projectMemberCount' => 0,
-            'workPackages' => $overview['workPackages'],
-            'workPackageCount' => $overview['workPackageCount'],
-            'taskCount' => $overview['taskCount'],
-            'averageProgress' => $overview['averageProgress'],
         ]);
     }
 
