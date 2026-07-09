d5df75f604fefb3525017cb1572472af62989efa
rdmge <rileydeman@gmail.com>
2026-07-09 15:26:05 +0200

    Bug-fix: Code error in src\Repository\Project\ProjectParticipantRepository.php

--- src/Repository/Project/ProjectParticipantRepository.php
+++ src/Repository/Project/ProjectParticipantRepository.php
@@ -18,36 +18,9 @@ class ProjectParticipantRepository extends ServiceEntityRepository
         parent::__construct($registry, ProjectParticipant::class);
     }
 
-    public function findActiveParticipant(Project $project, Profile $profile): ?ProjectParticipant
+    public function findActiveParticipant(Project $project, Profile $profile): \Doctrine\ORM\QueryBuilder
     {
-        return $this->createQueryBuilder('pp')
-            ->select([
-                'pp.id AS participant_id',
-                'IDENTITY(pp.profile) AS profile_id',
-                'p.displayName AS display_name',
-                'IDENTITY(pp.status) AS status_id',
-                's.name AS status_name',
-                'IDENTITY(pp.role) AS role_id',
-                'pp.joinedAt',
-                'pp.leftAt',
-            ])
-            ->innerJoin('pp.profile', 'p')
-            ->innerJoin('pp.status', 's')
-            ->innerJoin('pp.project', 'pr')
-            ->leftJoin('pp.role', 'r')
-            ->addSelect('r.name AS role_name')
-            ->where('pr.id = :projectId')
-            ->setParameter('projectId', $projectId)
-            ->orderBy('p.displayName', 'ASC')
-        return $this->createQueryBuilder('participant')
-            ->andWhere('participant.project = :project')
-            ->andWhere('participant.profile = :profile')
-            ->andWhere('participant.leftAt IS NULL')
-            ->setParameter('project', $project)
-            ->setParameter('profile', $profile)
-            ->getQuery()
-            ->getArrayResult();
-            ->getOneOrNullResult();
+
     }
 
     public function isActiveParticipant(Project $project, Profile $profile): bool
